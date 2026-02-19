2eda0d5ca4dfd8d8db5e61f211255fe0f8e07a54
MEKyR <Illodygin@gmail.com>
2026-02-20 00:15:47 +0500

    "Maximize it!" Task optimisation.

--- Python/Maximize It!.py
+++ Python/Maximize It!.py
@@ -4,10 +4,13 @@ K, M = map(int, input().split())
 lists = []
 for _ in range(K):
     lst = list(map(int, input().split()))
-    lists.append(lst[1:])
+    values = set((x*x) % M for x in lst[1:])
+    lists.append(values)
 max_value = 0
 for combo in product(*lists):
-    value = sum(x*x for x in combo) % M
-    if value > max_value:
-        max_value = value
+    value = sum(combo) % M
+    if value == M - 1:
+        print(M - 1)
+        exit()
+    max_value = max(max_value, value)
 print(max_value)
